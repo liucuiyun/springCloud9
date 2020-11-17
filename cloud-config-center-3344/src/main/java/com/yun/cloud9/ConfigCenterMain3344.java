4b2c911f0d25ffbd8791b7c3128eef0c3c0f892f
liu <cuiyun_l@yadea.com.cn>
2020-11-17 15:33:17 +0800

    SpringCloud Config配置中心学习

---
+++ cloud-config-center-3344/src/main/java/com/yun/cloud9/ConfigCenterMain3344.java
@@ -0,0 +1,18 @@
+package com.yun.cloud9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author yun
+ * @version 1.0
+ * @date 2020-11-14 16:02
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
---
+++ cloud-config-client-3355/src/main/java/com/yun/cloud9/controller/ConfigClientController.java
@@ -0,0 +1,24 @@
+package com.yun.cloud9.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yun
+ * @version 1.0
+ * @date 2020-11-16 14:49
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
