cb2da126247277568181b6cbcf91ca863f796747
liu <1756791148@qq.com>
2020-12-10 16:01:48 +0800

    Nacos持久化配置和集群配置学习

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/yun/cloud9/alibaba/NacosConfigClientMain3377.java
@@ -0,0 +1,19 @@
+package com.yun.cloud9.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author yun
+ * @version 1.0
+ * @date 2020-11-27 16:49
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosConfigClientMain3377 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class, args);
+    }
+}
